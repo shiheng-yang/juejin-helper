ff4385e9f07ee12e62054d3da721858d3537a0a2
unbalance <79517638+shiheng-yang@users.noreply.github.com>
2025-12-03 09:08:00 +0800

    Update env.js

--- workflows/utils/env.js
+++ workflows/utils/env.js
@@ -1,7 +1,7 @@
 const env = process.env || {};
 
 module.exports = {
-  /* 掘金Cookie */
+  /* 掘金Cookie  */
   /* 6653 undefined1234*/
   COOKIE: '__tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227376819242085910031%2522%252C%2522user_unique_id%2522%253A%25227376819242085910031%2522%252C%2522timestamp%2522%253A1717549596276%257D; store-region=cn-js; store-region-src=uid; passport_csrf_token=561e6240c2f7666ee2e45aa592e4900d; passport_csrf_token_default=561e6240c2f7666ee2e45aa592e4900d; _tea_utm_cache_2608={%22utm_campaign%22:%22liebian_invite%22}; _tea_utm_cache_2018={%22utm_campaign%22:%22liebian_invite%22}; n_mh=JH7bo6YchjHfkAFWg2LCNfVRDcKlCCkaEnFcpiUrd0c; sid_guard=4f915d6c8eaa7733c8f54e846687fad0%7C1742777434%7C31536000%7CTue%2C+24-Mar-2026+00%3A50%3A34+GMT; uid_tt=d5f3b167f9f5888a835767716e999da9; uid_tt_ss=d5f3b167f9f5888a835767716e999da9; sid_tt=4f915d6c8eaa7733c8f54e846687fad0; sessionid=4f915d6c8eaa7733c8f54e846687fad0; sessionid_ss=4f915d6c8eaa7733c8f54e846687fad0; is_staff_user=false; sid_ucp_v1=1.0.0-KGFmMTAxZGIzODM2YjgzNmM5ZjllNDhkMTIwMzhkYWI1NjllOTYyYTIKFwj6_LD1hM2WAhDa2IK_BhiwFDgHQPQHGgJsZiIgNGY5MTVkNmM4ZWFhNzczM2M4ZjU0ZTg0NjY4N2ZhZDA; ssid_ucp_v1=1.0.0-KGFmMTAxZGIzODM2YjgzNmM5ZjllNDhkMTIwMzhkYWI1NjllOTYyYTIKFwj6_LD1hM2WAhDa2IK_BhiwFDgHQPQHGgJsZiIgNGY5MTVkNmM4ZWFhNzczM2M4ZjU0ZTg0NjY4N2ZhZDA; csrf_session_id=c76c5989f3f94623d078be72db5e9ab4',
   /* 多用户掘金Cookie, 当有1名以上用户时填写, 支持同时最多可配置5名用户 */
